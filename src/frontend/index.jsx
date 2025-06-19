d6e8b65c0b0cfd7b8c9996b5b9cf32b7cc0a5041
Asmi Sharma <156397369+AsmiSharma017@users.noreply.github.com>
2025-06-19 20:38:46 +0530

    Update index.jsx

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
-import { invoke } from '@forge/bridge';
+import ForgeReconciler, { Heading, Lozenge, Text } from '@forge/react';
+import { invoke, view } from '@forge/bridge';
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    const getTheme = async () => {
+      const context = await view.getContext();
+      console.log(context); 
+      setTheme(context.theme.colorMode);
+    };
+    getTheme();
+  }, []);
+
   useEffect(() => {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
   }, []);
 
   return (
     <>
-      <Text>Hello world!</Text>
+      <Heading size="xlarge">Hello Charlie!</Heading>
       <Text>{data ? data : 'Loading...'}</Text>
+      <Text>Current theme: <Lozenge>{theme ? theme : 'Loading...'}</Lozenge></Text>
     </>
   );
 };
 
-ForgeReconciler.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ForgeReconciler.render(<App />);
